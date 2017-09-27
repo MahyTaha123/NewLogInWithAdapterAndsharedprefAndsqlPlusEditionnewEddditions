17ba3a205df793b15567aa1546a18448a2d1ec09
Maha123 <mahy_taha@yahoo.com>
2017-09-27 02:06:56 +0100

    3dCommit

--- app/src/main/java/com/example/maha/newloginwithadaptersharedprefsql/TvEdAdapter.java
+++ app/src/main/java/com/example/maha/newloginwithadaptersharedprefsql/TvEdAdapter.java
@@ -5,11 +5,15 @@ import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -31,13 +35,48 @@ public class TvEdAdapter extends ArrayAdapter<TvEdModel> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_raw, parent, false);
 
         }
-        TvEdModel model = getItem(position);
+        final TvEdModel model = getItem(position);
 
-        TextView tvSNotes = (TextView) convertView.findViewById(R.id.tvSNotes);
+        Button overflow = (Button) convertView.findViewById(R.id.overFlow);
+
+        final TextView tvSNotes = (TextView) convertView.findViewById(R.id.tvSNotes);
         TextView tvLoading = (TextView) convertView.findViewById(R.id.tvLoading);
         tvSNotes.setText(model.getNote());
         tvLoading.setText(model.getState());
 
+        overflow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                PopupMenu popupM = new PopupMenu(getContext(), view);
+                // popupMenu.inflate(R.menu.poupup_menu);
+                popupM.getMenuInflater()
+                        .inflate(R.menu.poupup_menu, popupM.getMenu());
+                popupM.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+
+
+                        switch (menuItem.getItemId()) {
+                            case R.id.remove:
+                                remove(model);
+                                notifyDataSetChanged();
+                                break;
+                            case R.id.toastMe:
+                                Toast.makeText(getContext(), tvSNotes.getText().toString(), Toast.LENGTH_LONG).show();
+                                break;
+
+                        }
+
+
+                        return true;
+                    }
+                });
+
+
+            }
+        });
 
         return convertView;
     }
